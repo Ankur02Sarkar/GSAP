07991a9ac31e1cc301e71392096fc493f894087f
Ankur-Raphacure <ankur@raphacure.com>
2024-07-20 12:30:02 +0530

    added default effects to all

--- src/app/effect080/Effect080.js
+++ src/app/effect080/Effect080.js
@@ -1,8 +1,27 @@
 "use client";
+import gsap from "gsap";
 import React from "react";
 
 const Effect080 = () => {
-  return <div>Effect080</div>;
+  const handleAnimate = () => {
+    gsap.fromTo(
+      ".heading",
+      { rotate: 0 },
+      { rotate: 360, duration: 1, clearProps: "all" }
+    );
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 gap-12">
+      <h1 className="heading text-4xl font-bold mb-4">Effect 080</h1>
+      <button
+        onClick={handleAnimate}
+        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition-colors duration-300"
+      >
+        Animate
+      </button>
+    </div>
+  );
 };
 
 export default Effect080;
--- src/app/script.js
+++ src/app/script.js
@@ -45,10 +45,29 @@ export default page;
     // Content for Effect<id>.js file
     const effectContent = `
 "use client";
+import gsap from "gsap";
 import React from "react";
 
 const Effect${paddedNumber} = () => {
-  return <div>Effect${paddedNumber}</div>;
+  const handleAnimate = () => {
+    gsap.fromTo(
+      ".heading",
+      { rotate: 0 },
+      { rotate: 360, duration: 1, clearProps: "all" }
+    );
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 gap-12">
+      <h1 className="heading text-4xl font-bold mb-4">Effect ${paddedNumber}</h1>
+      <button
+        onClick={handleAnimate}
+        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition-colors duration-300"
+      >
+        Animate
+      </button>
+    </div>
+  );
 };
 
 export default Effect${paddedNumber};
